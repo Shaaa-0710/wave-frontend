6b90aec798d362c5a1fb9bed7d7d4eb3ca789c83
Mohamed Shafeek M <mohamedshafeekk2007@gmail.com>
2025-12-27 14:55:31 +0530

    connect frontend to render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://wave-backend-upis.onrender.com/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
